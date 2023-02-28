47b6af93702bb9ddd15b24253b32bfff63c2fd28
Adrian Goding <godingadrian@gmail.com>
2023-02-27 19:14:23 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+const record = []
+
+function superbowlWin(record) {
+    const win = record.find(entry => entry.result === 'W');
+    if (win) {
+      return win.year;
+    } else {
+      return undefined;
+    }
+  }
